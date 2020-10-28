1897d98f77964cc7a7cef1c12e4fa3927234cda1
bepulver <73560933+bepulver@users.noreply.github.com>
2020-10-28 12:02:13 -0400

    Update AndGate.py

--- AndGate.py
+++ AndGate.py
@@ -5,7 +5,7 @@ import dimod
 import numpy
 import minorminer
 from dwave.system.samplers import DWaveSampler
-sampler = DWaveSampler(token='DEV-176c67d38a18f8e993b4bd11b99aae4d68ab85a3', solver='DW_2000Q_6')
+sampler = DWaveSampler(token="PLACEHOLDER", solver='DW_2000Q_6')
 
 #-----------------------------------------------QUBO------------------------------------------------------------------------------#
 

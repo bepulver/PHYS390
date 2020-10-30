09e8b308027ad9313b242fe92d82a40b9341458d
bepulver <73560933+bepulver@users.noreply.github.com>
2020-10-30 15:59:50 -0400

    Update AndGate.py

--- AndGate.py
+++ AndGate.py
@@ -14,7 +14,6 @@ Q[(0,1)] = 1
 Q[(1,2)] = -2
 Q[(0,2)] = -2
 Q[(2,2)] = 3
-Q
 
 #-----------------------------------------------COMPUTING-------------------------------------------------------------------------#
 
@@ -23,17 +22,10 @@ target_Q = dwave.embedding.embed_qubo(Q, embedding, sampler.adjacency, chain_str
 embedded_response = sampler.sample_qubo(target_Q, num_reads=10, answer_mode="raw")
 bqm = dimod.BinaryQuadraticModel.from_qubo(Q)
 response = dwave.embedding.unembed_sampleset(embedded_response, embedding, bqm)
-#print(type(response)) #dimod.sampleset.Sampleset
-#print(response)
 
 #-----------------------------------------------DIVIDING RESPONSE-----------------------------------------------------------------#
 
-energyArray = response.record.energy
 inputArray = response.record.sample
-#print(energyArray)
-#print(type(energyArray)) #numpy.ndarray
-#print(inputArray)
-#print(type(inputArray)) #numpy.ndarry
 
 #-----------------------------------------------TABULATION------------------------------------------------------------------------#
 
@@ -43,11 +35,11 @@ tabulatedData = {str: int}
 for i in inputArray:
     if str(i) in tabulatedData.keys():
         tabulatedData[str(i)] += 1
-        #print("I was in the keys already")
     else:
         tabulatedData[str(i)] = 1
-        #print("I got added to the keys just now")
     print(i)
 print(tabulatedData)
 
-#[1 0 0]': 1, '[0 1 0]': 5, '[0 0 0]': 2, '[1 1 1]': 2
+#Outputs for 10 samples and 1000 samples respectively
+#'[1 0 0]': 1, '[0 1 0]': 5, '[0 0 0]': 2, '[1 1 1]': 2
+#'[0 1 0]': 319, '[0 0 0]': 329, '[1 0 0]': 224, '[1 1 1]': 128
